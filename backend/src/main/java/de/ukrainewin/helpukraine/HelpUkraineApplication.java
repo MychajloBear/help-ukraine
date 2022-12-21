8aef64932f2d9511cb8be25e840a8e308079b1df
Michajlo Anti-Terror <vedmidmetal@ukr.net>
2022-12-21 10:09:41 +0100

    initial project

---
+++ backend/src/main/java/de/ukrainewin/helpukraine/HelpUkraineApplication.java
@@ -0,0 +1,13 @@
+package de.ukrainewin.helpukraine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpUkraineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpUkraineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/ukrainewin/helpukraine/HelpUkraineApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ukrainewin.helpukraine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpUkraineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
