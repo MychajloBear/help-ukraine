e3c6cfe4e8552c2062739d09d7342b436cdb269b
Michajlo Anti-Terror <vedmidmetal@ukr.net>
2023-01-18 23:44:02 +0100

    add flat entity

---
+++ backend/src/main/java/de/ukrainewin/helpukraine/model/Flat.java
@@ -0,0 +1,55 @@
+package de.ukrainewin.helpukraine.model;
+
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("foods")
+public class Flat {
+	@Id
+    private String id;
+    private String name;
+
+    public Flat(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flat food = (Flat) o;
+        return Objects.equals(id, food.id) && Objects.equals(name, food.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
