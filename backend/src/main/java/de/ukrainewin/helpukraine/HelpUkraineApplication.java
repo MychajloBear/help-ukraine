8e9e5b6896e970b712b9eea932e44d7cb03b4ae9
HELP-UKRAINE! <38041311+MychajloBear@users.noreply.github.com>
2023-12-18 13:43:40 +0100

    add springdoc annotation
    OpenAPIDefinition

--- backend/src/main/java/de/ukrainewin/helpukraine/HelpUkraineApplication.java
+++ backend/src/main/java/de/ukrainewin/helpukraine/HelpUkraineApplication.java
@@ -4,6 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+	@OpenAPIDefinition(
+		info = @Info(
+				title = "Test microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "3.0",
+				contact = @Contact(
+						name = "Maidan Ready",
+						email = "tutor@example.com",
+						url = "https://www.example.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.example.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Test Accounts microservice REST API Documentation",
+				url = "https://www.example.com/swagger-ui.html"
+		)
+)
+
 public class HelpUkraineApplication {
 
 	public static void main(String[] args) {
